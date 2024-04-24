652ed145817c1fe213148ac72d7ea502de934012
hadi.rafad <hadi.rafad@feathersoft.com>
2024-04-24 12:47:32 +0530

    usermodel added

--- app/core/models.py
+++ app/core/models.py
@@ -1,22 +1,35 @@
+from django.db import models  # noqa
+"""
+Database models.
+"""
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
-    PermissionsMixin
+    PermissionsMixin,
 )
 
+# Create your models here.
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **exctra_fields):
-        user    =   self.model(email=email, **exctra_fields)
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save(using=self._db)        
+        user.save(using=self._db)
+
         return user
-    
+
+
 class User(AbstractBaseUser, PermissionsMixin):
-    email       = models.EmailField(max_length=255, unique=True)
-    name        = models.CharField(max_length=255)
-    is_active   = models.BooleanField(default=True)
-    is_staff    = models.BooleanField(default=False)
-    
-    USERNAME_FIELD  =   'email'
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
