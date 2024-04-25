0c77720e352bb49f3a0f8bfae9e97326d0b53f4a
hadi.rafad <hadi.rafad@feathersoft.com>
2024-04-25 12:44:42 +0530

    documentation add

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -12,3 +12,30 @@ class ModelTests(TestCase):
         
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+        
+    def test_user_email_normalized(self):
+        sample_emails = [
+            ['abc1@EXAMPLE.com', 'abc1@example.com'],
+            ['Abc2@Example.com', 'Abc2@example.com'],
+            ['ABC3@EXAMPLE.com', 'ABC3@example.com'],
+            ['abc4@example.com', 'abc4@example.com']
+        ]
+        
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'aaa')
+            self.assertEqual(user.email, expected)
+            
+    def test_new_user_without_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', '12345')
+            
+    def test_create_superuser(self):
+        email       =   "test@gmail.com"
+        password    =   "12345"
+        user        =   get_user_model().objects.create_superuser(
+            email       =   email, 
+            password    =   password,
+        )
+        
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
