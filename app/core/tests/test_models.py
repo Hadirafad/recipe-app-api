99e5ba04c22837e36cc410758ed3091b6912ce41
hadi.rafad <hadi.rafad@feathersoft.com>
2024-05-01 17:14:02 +0530

    recipe app and apis

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from decimal import Decimal
+
+from core import models
 
 class ModelTests(TestCase):
     def test_create_user_with_email_success(self):
@@ -39,3 +42,19 @@ class ModelTests(TestCase):
         
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+        
+    def test_create_recipe(self):
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'TestPass123'
+        )
+        
+        recipe = models.Recipe.objects.create(
+            user=user, 
+            title='Coffee', 
+            time_minutes=5, 
+            price=Decimal('5.50'),
+            description='Coffee desc'
+            )
+        
+        self.assertEqual(str(recipe), recipe.title)
